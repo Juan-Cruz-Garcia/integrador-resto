5e18e29b2811db19f437c8c67148cbf4e2b3a525
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-06-29 08:39:12 -0300

    change info

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -1,38 +1,36 @@
-
 <footer class="bg-dark text-white py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <h5>Información de Contacto</h5>
-          <p>Dirección: 123 Calle Principal, Ciudad</p>
-          <p>Teléfono: +123 456 789</p>
-          <p>Email: info@tudominio.com</p>
-        </div>
-        <div class="col-md-4">
-          <h5>Enlaces Rápidos</h5>
-          <ul class="list-unstyled">
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Productos</a></li>
-            <li><a href="#">Servicios</a></li>
-            <li><a href="#">Contacto</a></li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-          <h5>Síguenos en Redes Sociales</h5>
-          <ul class="list-unstyled">
-            <li><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
-            <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
-            <li><a href="#"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
-          </ul>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h5>Información de la Taberna</h5>
+        <p>Dirección: 123 Camino del Dragón, Ciudad de Aventuras</p>
+        <p>Teléfono: +123 456 789 (Hechicero del Portal)</p>
+        <p>Email: contacto@tabernadelrefugio.com</p>
       </div>
-      <hr class="my-4">
-      <p class="text-center">&copy; 2024 - Todos los derechos reservados</p>
+      <div class="col-md-4">
+        <h5>Enlaces Rápidos</h5>
+        <ul class="list-unstyled">
+          <li><a href="{{ route('web.landingpage') }}">Inicio</a></li>
+          <li><a href="{{ route('web.dishes.index') }}">Platos</a></li>
+          <li><a href="#">Hechizos</a></li>
+          <li><a href="#">Aventuras</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <h5>Síguenos en las Redes de los Reinos</h5>
+        <ul class="list-unstyled">
+          <li><a href="https://www.facebook.com" target="_blank">Grimorio de Caralibro</a></li>
+          <li><a href="https://www.twitter.com" target="_blank">Trino del Bardo</a></li>
+          <li><a href="https://www.instagram.com" target="_blank">Retratos del Mago</a></li>
+          <li><a href="https://www.linkedin.com" target="_blank">Red de Mercaderes</a></li>
+        </ul>
+      </div>
     </div>
-  </footer>
-  
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+z1IMk/933+STs4lTmi/Z3nJ2ii4Y5j8o4y0p3h" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-  
+    <hr class="my-4">
+    <p class="text-center">&copy; 2024 - Todos los derechos reservados. El Refugio del Pecador, Taberna de Aventuras.</p>
+  </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+z1IMk/933+STs4lTmi/Z3nJ2ii4Y5j8o4y0p3h" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
