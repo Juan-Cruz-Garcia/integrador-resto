80c7c3c5843d4a2408fe97e308940a673ab877da
Juan Cruz <juan.garcia01@davinci.edu.ar>
2024-06-30 00:50:51 -0300

    _construct, disponibility, wrok in progress index

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -4,21 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Dish;
+use App\Models\Category;
 
 class DishController extends Controller
 {
+    // Variable para almacenar las categorías
+    protected $categories;
+
+    //permite disponibilidad en todas las vistas instaciadas de este controlador
+    public function __construct()
+    {
+        $this->categories = Category::all();
+        view()->share('categories', $this->categories); // se pasan las categorías a todas las vistas.
+    }
+
     public function landingpage()
     {
-         // Obtener 3 platos al azar
-         $randomDishes = Dish::inRandomOrder()->limit(3)->get();
-        return view('web.welcome',compact('randomDishes'));
+        // Obtener 3 platos al azar que estén disponibles
+        $randomDishes = Dish::where('is_available', 1)->inRandomOrder()->limit(3)->get();
+        return view('web.welcome', compact('randomDishes'));
     }
+
 
     public function index()
     {
-        $dishes = Dish::paginate(6);
+        // Filtrar los platos que están disponibles
+        $dishes = Dish::where('is_available', 1)->paginate(6);
+        if (request()->has($orderBy)) {
+            $dishes = Dish::where('is_available', 1)->-orderBy($orderBy)>paginate(6);
+
+        }
+
         return view('web.dishes.index', compact('dishes'));
     }
+
 
     public function show($id)
     {
